60c6e8de55390dc627573ebf38f964b35a45cca6
LuCod3 <lukas.zwak@gmail.com>
2020-06-10 14:06:48 +0200

    change file path

--- app/javascript/packs/sidebar.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "25%";
-    document.getElementById("main").style.marginLeft = "25%";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
