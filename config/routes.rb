315e508ec77e80bfd28cd330bc9b59d1c6e96488
ttzv <tomasz.zwak@gmail.com>
2020-06-19 11:52:07 +0200

    Add link to sync with ldap, add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'item#index'
   get 'item', to: 'item#index'
   get 'item/ad_users', to: 'ad_users#index'
   get 'item/ad_users/all', to: 'item#all_users'
+  get 'item/ad_users/reload', to: 'ad_users#reload'
   post 'item/ad_users/all', to: 'item#all_users_filtered'
 end
