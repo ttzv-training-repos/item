182e9895ede527e4a595c058152df29e71fe021a
LuCod3 <lukas.zwak@gmail.com>
2020-06-19 13:30:16 +0200

    recommit from before pull request

--- app/assets/javascripts/table_gen.js
+++ app/assets/javascripts/table_gen.js
@@ -1,15 +1,48 @@
-function generate_table() {
-
+function generate_table(data) {
   var table = document.getElementById("myTable");
-  var row = table.insertRow(0);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  var cell3 = row.insertCell(2);
+  if(row >0){
+    const headers = data["headers"];
+    const UserData = data["users"];
 
-  cell1.innerHTML = "new cell1";
-  cell2.innerHTML = "new cell2";
-  cell3.innerHTML = "new cell3";
+    
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+
+    cell1.innerHTML = headers.samaccountname;
+    cell2.innerHTML = headers.name;
+    cell3.innerHTML = headers.whencreated;
+    
+    for (var i=0; i<= 500; i++) {
+      
+      var row = table.insertRow(i+1);
+      var cell4 = row.insertCell(0);
+      var cell5 = row.insertCell(1);
+      var cell6 = row.insertCell(2);
+      cell4.innerHTML = UserData[i].samaccountname;
+      cell5.innerHTML = UserData[i].name;
+      cell6.innerHTML = UserData[i].whencreated;
+    }
+  }
+  else{
+    alert("You already requested data!"+ "\n" +"Refresh site to do it again");
+  }
 
   table.setAttribute("border", "1");
-  table.style.backgroundColor = "lightgrey";
+  table.style.backgroundColor = "white";
 }
+/*
+function betterTableGen(){
+  $(document).ready( function () {
+    $('myTable').DataTable({
+      "ajax": "http://10.0.1.117:3000/item/ad_users/all",
+      "columns" : [
+        {"users":  "samacountname"},
+        {"users":  "samacountname"},
+        {"users":  "samacountname"}
+      ]
+    });
+  } );
+}
+*/
