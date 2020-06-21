792a601d8fa2f58cdf641ef5e820022ad731d9db
ttzv <teyed1fy@gmail.com>
2020-06-21 20:01:57 +0200

    Add simple cart-like temporary storage for selected users

---
+++ test/controllers/user_holders_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UserHoldersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/user_holder_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UserHolderTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
