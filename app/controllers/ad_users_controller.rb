d8fcae9c7255918fd481764168ae7a033bf49184
ttzv <tomasz.zwak@gmail.com>
2020-06-22 15:58:23 +0200

    Start making table partial reusable

--- app/controllers/ad_users_controller.rb
+++ app/controllers/ad_users_controller.rb
@@ -1,5 +1,11 @@
 class AdUsersController < ApplicationController
 
+  def index
+    @headers = AdUserHeader.en_headers
+    @users = AdUser.all.limit(10)
+    @users = [@users] unless @users.length > 0
+  end
+
   def reload
     ldap_sync = LdapServices::LdapSync.new(LdapServices::LdapConn.new)
     ldap_sync.sync
--- app/models/ad_user_header.rb
+++ app/models/ad_user_header.rb
@@ -1,4 +1,5 @@
 class AdUserHeader < ApplicationRecord
+
   def self.en_headers
     headers = Hash.new
     AdUserHeader.pluck(:name, :name_en).each { |entry| headers.merge!(entry.first => entry.last) }
