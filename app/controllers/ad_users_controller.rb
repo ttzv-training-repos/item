425eb1f3bb535f079c9a030b11a280fb6c3349f7
ttzv <tomasz.zwak@gmail.com>
2020-06-29 13:21:47 +0200

    Cleanup routes and fix paths

--- app/controllers/ad_users_controller.rb
+++ app/controllers/ad_users_controller.rb
@@ -10,7 +10,7 @@ class AdUsersController < ApplicationController
   def reload
     ldap_sync = LdapServices::LdapSync.new(LdapServices::LdapConn.new)
     ldap_sync.sync
-    redirect_to(item_ad_users_path)
+    redirect_to(ad_users_path)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,19 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'item#index'
 
-  resource :item do
-    resources :ad_users
-  end
   get 'item', to: 'item#index'
-  get 'item/ad_users', to: 'ad_users#index'
-  get 'item/ad_users/reload', to: 'ad_users#reload'
-  post 'item/user_holders/select', to: 'user_holders#select'
-  post 'item/user_holders/clear', to: 'user_holders#clear'
-  get 'item/user_holders/qty', to: 'user_holders#qty'
-  get 'item/user_holders/content', to: 'user_holders#content'
-  get 'item/user_holders', to: 'user_holders#index'
-  get 'item/mails', to: 'mails#index'
+  scope '/item' do
+    resources :ad_users, only: [:index]
+    resources :user_holders, only: [:index]
+    get '/ad_users/reload', to: 'ad_users#reload'
+  end
+
+  scope '/item/user_holders' do
+    post '/select', to: 'user_holders#select'
+    post '/clear', to: 'user_holders#clear'
+    get '/qty', to: 'user_holders#qty'
+    get '/content', to: 'user_holders#content'
+  end
+
+  resources :mails, only: [:index]
 end
