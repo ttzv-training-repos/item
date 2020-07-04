34cd773249177d54358a8814fe87234cc43cf89e
ttzv <teyed1fy@gmail.com>
2020-07-04 11:49:26 +0200

    Prepare buttons controlling selection in AdUsers view

--- app/assets/javascripts/ad_users.js
+++ app/assets/javascripts/ad_users.js
@@ -1,5 +1,19 @@
+var dataTable;
 $(document).ready( function () {
     dataTable = $( "#ad_users" ).DataTable({
         select: true
     });
+    
 });
+
+
+
+$('#users-select').click(handleSelection)
+$('#users-clear').click(handleDeselection)
+
+function handleSelection(params) {
+    console.log(dataTable.rows({selected: true}).data());
+}
+function handleDeselection(params) {
+    console.log(dataTable.rows({selected: true}).data());
+}
