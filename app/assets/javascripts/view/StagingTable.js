88b8578441990a62abd49736882030577d67779c
ttzv <teyed1fy@gmail.com>
2020-08-30 10:56:00 +0200

    Refactor StagingTable class, add popups

--- app/assets/javascripts/view/StagingTable.js
+++ app/assets/javascripts/view/StagingTable.js
@@ -1,77 +1,154 @@
 class StagingTable{
+
+    /**
+     * Class used to generate "staging" table where all prepared messages can be viewed before sending.
+     * @param  {PreparedMessageContainer} content PreparedMessageContainer object that responds to getJson() method.
+     */
     constructor(content){
-        this._content = content
+        if (!content) throw new Error("Staging Table cannot be created without Prepared Messages");
+        this._content = content.getJson().message_request.messages;
+        this._headerRow = null;
+ 
+        this.buildTableScaffold();
+        this.addHtmlAttributes();
     }
 
-    //Returns data needed for displaying table in staging area, after clicking send button.
-    //By default template names are used in table headers. this behavior can be changed by providing option -  by: "mails" (not implemented)
-    getStagingContent(options={}){
-        let headers = new Array();
-        let content = new Array();
-        let row = 0;
-        if (this._content.length > 0){
-            this._content.forEach( message => {
-                let title = message.template.title
-                if (!headers.includes(title)){
-                    headers.push(title)    
-                } 
-                row %= User.all.length;
-                if (!content[row]) content.push([])
-                content[row].push(message)
-                row++;
+    buildTableScaffold(){
+        this._table = document.createElement('table'),
+        this._thead = document.createElement('thead'),
+        this._theadRow = document.createElement('tr'),
+        this._tbody = document.createElement('tbody');
+        this._table.appendChild(this._thead);
+        this._thead.appendChild(this._theadRow);
+        this._table.appendChild(this._tbody);
+    }
+
+    addHtmlAttributes(){
+        this._table.classList.add('table', 'table-striped', 'table-bordered', 'table-hover-cells');
+        this._table.setAttribute('id', 'stagingTable');
+    }
+
+    /**
+     * Sorts prepared messaged based on given criteria and returns Map object with sorted content;
+     * @param  {} option criteria to sort by, use {by: "subject"} or {by: "recipient"}
+     */
+    sort(option){
+        let tableMap = new Map();
+        if(option.by === "subject"){
+            let uniqueSubjects = [...new Set(this._content.map(message => message.subject))];
+            uniqueSubjects.forEach(subject => {
+                tableMap.set(subject, this._content.filter(message => message.subject === subject));
             });
-            return {headers: headers, content: content}
-        } else {
-            throw "Cant create staging content, no Messages and/or Users present"
+            return tableMap;
+        }
+        if(option.by === "recipient"){
+            let uniqueRecipients = [...new Set(this._content.map(message => message.recipient))]
+            uniqueRecipients.forEach(recipient => {
+                tableMap.set(recipient, this._content.filter(message => message.recipient === recipient));
+            });
+            return tableMap;
         }
     }
 
-    renderStagingAreaTable(stagingContent){
-        if (stagingContent.headers === null) throw "Headers are required to build a table"
-        if (stagingContent.content === null) throw "Content is null, can't build an empty table"
-
-        let table = document.createElement('table'),
-            thead = document.createElement('thead'),
-            theadRow = document.createElement('tr'),
-            tbody = document.createElement('tbody');
-            
-        table.classList.add('table', 'table-striped', 'table-bordered', 'table-hover-cells');
-        table.setAttribute('id', 'stagingTable')
-            
-        table.appendChild(thead);
-        table.appendChild(tbody);
-
-        stagingContent.headers.forEach( header => {
-            let headerCell = document.createElement('th');
-            let headerCellText = document.createTextNode(header);
-            headerCell.appendChild(headerCellText);
-            theadRow.appendChild(headerCell);
-        });
-        thead.appendChild(theadRow);
+    renderStagingAreaTable(tableMap){
+        this.buildHeaderContent(tableMap);
                 
-        stagingContent.content.forEach( rowContent => {
-            let tr = tbody.insertRow();
-            rowContent.forEach(cellContent => {
-                let cell = tr.insertCell();
-                cell.appendChild(document.createTextNode(cellContent.user.ad_users_mail));
-                cell.addEventListener('click', () => {
-                    console.log(cellContent);
-                    $('#staging-preview-area').empty().append(cellContent.content);
-                })
-            });
-        });
+        let maxRows = this.maxRows(tableMap);
+        let mapLength = this.mapLength(tableMap);
+        let mapValues = this.mapValues(tableMap);
+        let tr = null;
+        let innerIndex = 0;
 
-        return table;
+        for (let index = 0; index < mapLength*maxRows; index++) {
+            if (index % mapLength === 0) {
+                tr = this._tbody.insertRow(); 
+            }
+            let cell = tr.insertCell();
+            let message = this.getMessageFromMapValues(mapValues, index % mapLength, innerIndex);
+            this.buildCellContentFromMessage(cell, message);
+            if (index % mapLength === mapLength - 1){
+                innerIndex++;
+            } 
+        }
+        return this._table;
     }
 
-    render(){
-        let stagingContent = this.getStagingContent();
-        return this.renderStagingAreaTable(stagingContent);
+    /** 
+     * Return length of longest nested Array in Map.
+     * @param  {} tableMap Map with Arrays as values
+     */
+    maxRows(tableMap){
+        return [...tableMap.values()].reduce((max, cur) => {
+            if(cur.length > max){
+                max = cur.length;
+            }
+            return max;
+        },0)
+    }
+  
+    /** 
+     * Returns number of keys stored on Map
+     * @param  {} anyMap
+     */
+    mapLength(anyMap){
+        return [...anyMap.keys()].length;
     }
 
-    empty(){
+    /**
+     * Returns Array of values present in Map
+     * @param  {} anyMap
+     */
+    mapValues(anyMap){
+        return [...anyMap.values()];
+    }
+
+    isEmpty(){
         return this._content.length <= 0;
     }
 
+    getMessageFromMapValues(map, arrNo, index){
+        if(map[arrNo]){
+            return map[arrNo][index] ?? null; 
+        }
+        return null;
+    }
+
+    buildHeaderContent(tableMap){
+        tableMap.forEach((messages, subject) => {
+            let headerCell = document.createElement('th');
+            let headerCellText = document.createTextNode(subject);
+            headerCell.appendChild(headerCellText);
+            this._theadRow.appendChild(headerCell);
+        });
+    }
+
+    buildCellContentFromMessage(cell, message){
+        if(message){
+            cell.appendChild(document.createTextNode(message.recipient));
+            this.handleCellClick(cell, message);
+            this.handleCellHover(cell, message);
+            this.buildCellPopover(cell, message);
+        }
+    }
+
+    handleCellClick(cell, message){
+        cell.addEventListener('click', () => {
+            console.log(message);
+        });
+    }
+
+    handleCellHover(cell, message){
+        cell.addEventListener('mouseover', (e) => {
+            setTimeout(() => {
+                $('.popover-body').html(message.content);
+            }, 50);
+        })
+    }
     
+    buildCellPopover(cell, message){
+        cell.dataset.toggle = "popover";
+        cell.dataset.trigger = "hover";
+        cell.dataset.content = message.content;
+        cell.classList.toggle("staging-popover");
+    }
 }
--- app/assets/javascripts/view/staging.js
+++ app/assets/javascripts/view/staging.js
@@ -1,8 +1,17 @@
 $('#stagingModal').on('shown.bs.modal', function() {
     if (messageContainer !== null){
-        let stagingTable = new StagingTable(messageContainer.getPreparedMessages());
-        stagingTable.empty() ? $('#send-request').attr('disabled', true) : $('#send-request').attr('disabled', false)
-        let table = stagingTable.render();
+        let stagingTable = new StagingTable(messageContainer);
+        stagingTable.isEmpty() ? $('#send-request').attr('disabled', true) : $('#send-request').attr('disabled', false)
+        let table = stagingTable.renderStagingAreaTable(
+            stagingTable.sort(
+                {by: "recipient"}
+                ));
         $('#staging-table-area').empty().append(table);
+        $('.staging-popover').popover({
+            html: true
+        });
     }
 })
+$('#stagingModal').on('hide.bs.modal', function() {
+    $('#staging-table-area').empty();
+});
