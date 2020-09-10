4ab6de2e2c2d9ce9fbc6ba8bc405ee258adfbb7c
ttzv <tomasz.zwak@gmail.com>
2020-09-10 13:55:35 +0200

    Implement actual upsert

--- app/models/template_tagging.rb
+++ app/models/template_tagging.rb
@@ -3,6 +3,11 @@ class TemplateTagging < ApplicationRecord
   belongs_to :template_tag
 
   def self.upsert(hash)
-    TemplateTagging.create(hash) if TemplateTagging.find_by(hash).nil?
+    template_tagging = TemplateTagging.find_by(hash)
+    if TemplateTagging.nil?
+      TemplateTagging.create(hash) 
+    else
+      template_tagging.update(hash)
+    end
   end
 end
