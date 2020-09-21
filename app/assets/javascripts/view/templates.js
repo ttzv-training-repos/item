7a1d0d5d4b2d40cd1a093b4785b8dc343bce799a
ttzv <tomasz.zwak@gmail.com>
2020-09-21 14:43:43 +0200

    Fix bug with popover staying visible after its element does not exist anymore

--- app/assets/javascripts/view/templates.js
+++ app/assets/javascripts/view/templates.js
@@ -2,8 +2,21 @@ if( $('body').attr('class') == 'templates edit'){
     let templateEditor = new TemplateEditor();
     templateEditor.build();
     console.log("template editor")
+    hideOldTagPopovers();
+    initializeTagPopovers();
+}
+
+function hideOldTagPopovers() {
+    $('#tagSelectionArea').on('ajax:beforeSend', function (event) {
+        console.log("before send")
+        $('li[data-tag-name]').popover('hide');
+    })
+}
+
+function initializeTagPopovers() {
     $('li[data-tag-name]').popover({
-        trigger: 'hover'
+        trigger: 'hover',
+        delay: { "show": 1000, "hide": 100 }
     });
 }
 
