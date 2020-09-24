b3cc6a7a4c85eef177bd38b968fbd4a9e69e45d4
ttzv <tomasz.zwak@gmail.com>
2020-09-24 15:28:28 +0200

    add dependent: :destroy to tag_custom_mask

--- app/models/template_tagging.rb
+++ app/models/template_tagging.rb
@@ -1,7 +1,7 @@
 class TemplateTagging < ApplicationRecord
   belongs_to :template
   belongs_to :itemtag
-  has_one :tag_custom_mask
+  has_one :tag_custom_mask, dependent: :destroy
 
   def self.upsert(hash)
     template_tagging = TemplateTagging.find_by(hash)
