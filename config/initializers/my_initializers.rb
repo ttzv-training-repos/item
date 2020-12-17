354bc326d17cb21e4e38b815dba0be5ac4761bcc
ttzv <teyed1fy@gmail.com>
2020-12-17 20:03:36 +0100

    Extend String with colorization

--- config/initializers/my_initializers.rb
+++ config/initializers/my_initializers.rb
@@ -1,3 +1,8 @@
 MASKED_PASSWORD = '!@#$^%*(!@'
 
 ActionMailer::Base.add_delivery_method :gmail_adapter, GoogleApiServices::GmailAdapter
+
+#for my convenience =)
+#file below contains a class that extends String and allows for changing output color
+#located under lib/
+require 'colorize.rb'
