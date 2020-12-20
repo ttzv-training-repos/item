7b358129ef9b15b495b6ce45ca60a05bbc0b3474
ttzv <teyed1fy@gmail.com>
2020-12-20 18:24:14 +0100

    Start implementing sms

--- app/controllers/sms_gateway_controller.rb
+++ app/controllers/sms_gateway_controller.rb
@@ -1,9 +1,17 @@
 class SmsGatewayController < ApplicationController
+  require 'smsapi'
 
   def index
-    @sender = "123_placeholder"
+    sms_setting = current_user.sms_setting
+    if sms_setting
+      @sender = sms_setting.sender_name
+      client = Smsapi::Client.new(sms_setting.token)
+      @smsapi_points = client.credits.balance
+    else
+      @sender = "SMSApi configuration missing"
+      @smsapi_points = "SMSApi configuration missing"
+    end
     @templates = Template.where(category: "sms")
-    @smsapi_points = 999
   end
 
   def send_request
